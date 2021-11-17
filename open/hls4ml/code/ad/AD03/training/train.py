d0fc502a87fa4bcb19c4b1a213bf4f282985a189
Aidan Yokuda <74520882+AidanYok@users.noreply.github.com>
2021-11-16 23:59:39 -0800

    Update train.py

--- open/hls4ml/code/ad/AD03/training/train.py
+++ open/hls4ml/code/ad/AD03/training/train.py
@@ -13,8 +13,8 @@ import setGPU
 import os
 import tensorflow as tf
 gpus = tf.config.experimental.list_physical_devices('GPU')
-# for gpu in gpus:
-#   tf.config.experimental.set_memory_growth(gpu, True)
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
 import glob
 import sys
 import time
